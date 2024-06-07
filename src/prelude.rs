f6d481fc09edab1e48d18bb298fcc076749b6783
Tej Magar <tejmagar20510@gmail.com>
2024-06-07 20:54:51 +0545

    Add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+pub use crate::core::forms::FileFieldShortcut;
+pub use crate::core::request::Request;
+pub use crate::core::response::Response;
+pub use crate::core::response::status::ResponseStatus;
+pub use crate::core::response::HttpResponse;
+pub use crate::core::response::JsonResponse;
+pub use crate::core::shortcuts::SingleText;
